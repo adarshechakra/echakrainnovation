6547cc4d80fc7d392b8ab2d3949c0de80b5cfc88
adarshechakra <minivetexim.help@gmail.com>
2021-02-15 19:35:56 +0530

    updateeve

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 config.serve_static_assets = true
 config.assets.compile = true
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  # config.action_controller.asset_host = 'http://assets.example.com adarsh'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
